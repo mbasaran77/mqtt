f0cf3f1644f9f6c05724d047450612abf5607f2b
mbasaran77 <mbasaran77@gmail.com>
2018-08-04 11:24:15 +0300

    ibm-iot rev

--- ibm_iot.py
+++ ibm_iot.py
@@ -1,23 +1,26 @@
+import sys
+import time
+import json
+import paho.mqtt.client as mqtt
+
+# import ibmiotf.device
+
+host = 'oqpqg5.messaging.internetofthings.ibmcloud.com'
+client_id = 'd:oqpqg5:Sensor:TempSensor_1'
+username = 'use_token_auth'
+password = 'token'
+topic = 'iot-2/evt/temperature/fmt/json'
 
 
-import ibmiotf.device
 
-orgId = 'kfiraw'
-deviceType = 'plc'
-deviceId = 'plc_01'
-authMethod = 'token'
-authToken = 'iot7791ioT'
+client = mqtt.Client(client_id)
+client.username_pw_set(username, password)
+client.connect(host, 1883, 60)
 
+while True:
+  client.publish(topic, json.dumps({'t':40, 'h':45}))
+  print('message published')
+  time.sleep(5)
 
-try:
-  options = {
-    "org": orgId,
-    "type": deviceType,
-    "id": deviceId,
-    "auth-method": authMethod,
-    "auth-token": authToken,
-    "clean-session": True
-  }
-  client = ibmiotf.device.Client(options)
-except ibmiotf.ConnectionException as e:
-  print(e)
+client.loop()
+client.disconnect()
