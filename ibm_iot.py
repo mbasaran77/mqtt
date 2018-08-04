936afba9ee9753c0b7ee126bb0e88f68a05bd675
mbasaran77 <mbasaran77@gmail.com>
2018-08-04 13:43:37 +0300

    ibm-iot ok

--- ibm_iot.py
+++ ibm_iot.py
@@ -6,9 +6,9 @@ import paho.mqtt.client as mqtt
 # import ibmiotf.device
 
 host = 'oqpqg5.messaging.internetofthings.ibmcloud.com'
-client_id = 'd:oqpqg5:Sensor:TempSensor_1'
-username = 'use_token_auth'
-password = 'token'
+client_id = 'd:oqpqg5:temp_sensor_01:ts_02'
+username = 'use-token-auth'
+password = 'gdfKl!FZTaX9!!_Esm'
 topic = 'iot-2/evt/temperature/fmt/json'
 
 
@@ -16,11 +16,11 @@ topic = 'iot-2/evt/temperature/fmt/json'
 client = mqtt.Client(client_id)
 client.username_pw_set(username, password)
 client.connect(host, 1883, 60)
-
-while True:
+i = 0
+while i<5:
   client.publish(topic, json.dumps({'t':40, 'h':45}))
   print('message published')
   time.sleep(5)
-
+  i += 1
 client.loop()
 client.disconnect()
