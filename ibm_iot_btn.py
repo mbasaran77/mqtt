82daf6cdaefd4072430892c09d9e4f433b9a96f7
mbasaran77 <mbasaran77@gmail.com>
2018-08-05 08:09:38 +0300

    ibmiot btn bulb

--- ibm_iot_btn.py
+++ ibm_iot_btn.py
@@ -6,10 +6,10 @@ import paho.mqtt.client as mqtt
 # import ibmiotf.device
 
 host = 'oqpqg5.messaging.internetofthings.ibmcloud.com'
-client_id = 'd:oqpqg5:temp_sensor_01:ts_02'
+client_id = 'd:oqpqg5:Buttons:Button'
 username = 'use-token-auth'
-password = 'gdfKl!FZTaX9!!_Esm'
-topic = 'iot-2/evt/temperature/fmt/json'
+password = 'H1qNC5-TzKt_8fFV*j'
+topic = 'iot-2/evt/button/fmt/json'
 
 client = mqtt.Client(client_id)
 client.username_pw_set(username, password)
@@ -17,9 +17,9 @@ client.connect(host, 1883, 60)
 i = 0
 
 
-
 while i < 5:
-    client.publish(topic, json.dumps({'t': 40, 'h': 45}))
+    payload = {"Button": "on"}
+    client.publish(topic, json.dumps(payload))
     print('message published')
     time.sleep(5)
     i += 1
---
+++ ibm_iot_bulb.py
@@ -0,0 +1,49 @@
+import sys
+import time
+import json
+import paho.mqtt.client as mqtt
+
+# import ibmiotf.device
+
+host = 'oqpqg5.messaging.internetofthings.ibmcloud.com'
+client_id = 'd:oqpqg5:Bulbs:Bulb'
+username = 'use-token-auth'
+password = 'YsbjUOoOrXJx8*h&ek'
+topic = 'iot-2/cmd/state/fmt/json'
+
+
+def on_connect(client, userdata, flags, rc):
+    print("connected with result code " + str(rc))
+
+
+def on_message(client, obj, msg):
+    button = json.load(msg.payload)["state"]
+    if button == "on":
+        print("btn on")
+    else:
+        print("btn off")
+
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    print(button)
+
+
+def on_publish(client, obj, mid):
+    print("mid: " + str(mid))
+
+
+def on_subscribe(client, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+
+def on_log(client, obj, level, string):
+    print(string)
+
+
+client = mqtt.Client(client_id)
+client.username_pw_set(username, password)
+client.connect(host, 1883, 60)
+client.subscribe(topic)
+client.on_connect = on_connect
+client.on_message = on_message
+client.loop_forever()
+client.disconnect()
